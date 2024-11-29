e2768a7b607529cdaa8882c73fdad9a42e2597c9
Gin <142237326+ZeroIsStar@users.noreply.github.com>
2024-11-29 20:39:48 +0800

    Update __init__.py

--- base_model/__init__.py
+++ base_model/__init__.py
@@ -1,1 +1,12 @@
+from base_model.FCN8s import FCN8s
+from base_model.FCN16s import FCN16s
+from base_model.FCN32s import FCN32s
+from base_model.deeplabV3_plus import DeepLabV3plus
+from base_model.Unet import UNet
+from base_model.UnetPP import UNet_Nested
+from base_model.transunet import transNet
+from base_model.EfficientPyramidMamba import EfficientPyramidMamba
+from base_model.A2FPN import A2FPN
+from base_model.CMTFnet import CMTFNet
+from base_model.UFPN import model
 
